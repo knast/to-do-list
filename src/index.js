b228973db932e7496d2d8f5f0dc1ceb16261c214
knast <knast808@gmail.com>
2023-03-29 22:55:29 +0200

    Add buttons and some styling on the website

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 
 (() => {
+
     const body = document.querySelector('body');
     const projectsTab = document.createElement('div');
     projectsTab.classList.add('projects');
@@ -9,5 +10,17 @@
     tasksTab.classList.add('tasks');
     body.appendChild(tasksTab);
 
+    const projects = document.querySelector('.projects');
+    const tasks = document.querySelector('.tasks');
+
+    const projectsBtn = document.createElement('button');
+    projectsBtn.classList.add('add-projects');
+    projectsBtn.textContent = '+Add Project';
+    projects.appendChild(projectsBtn);
+
+    const tasksBtn = document.createElement('button');
+    tasksBtn.classList.add('add-tasks');
+    tasksBtn.textContent = '+Add Task';
+    tasks.appendChild(tasksBtn);
 
 })()
