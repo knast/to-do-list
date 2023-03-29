58103b6c60b9fd0a432a64502eafb46d4ebf45a2
knast <knast808@gmail.com>
2023-03-29 22:30:28 +0200

    Add projects and tasks sections

--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
 
 (() => {
-    
+    const body = document.querySelector('body');
+    const projectsTab = document.createElement('div');
+    projectsTab.classList.add('projects');
+    body.appendChild(projectsTab);
+
+    const tasksTab = document.createElement('div');
+    tasksTab.classList.add('tasks');
+    body.appendChild(tasksTab);
 
 
 })()
